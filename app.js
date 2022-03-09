a8ed8dbd973fe28ed3476d8bdfde42ce8142fdde
heiume <chauyihei@gmail.com>
2022-03-10 02:27:05 +0800

    update3

--- app.js
+++ app.js
@@ -1,37 +1,6 @@
-const mapData = {
-  minX: 1,
-  maxX: 14,
-  minY: 4,
-  maxY: 12,
-  blockedSpaces: {
-    "7x4": true,
-    "1x11": true,
-    "12x10": true,
-    "4x7": true,
-    "5x7": true,
-    "6x7": true,
-    "8x6": true,
-    "9x6": true,
-    "10x6": true,
-    "7x9": true,
-    "8x9": true,
-    "9x9": true,
-  },
-};
-
 // Options for Player Colors... these are in the same order as our sprite sheet
-const playerColors = ["blue", "red", "orange", "yellow", "green", "purple"];
-
-`const playerEar = ["1", "2", "3", "4"];`
-  `const playerHead = ["1", "2", "3", "4"];`
-
-//Misc Helpers
-function randomFromArray(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-function getKeyString(x, y) {
-  return `${x}x${y}`;
-}
+const playerEar = ["1", "2", "3", "4"];
+const playerHead = ["1", "2", "3", "4"];
 
 function createName() {
   const prefix = randomFromArray([
@@ -73,47 +42,6 @@ function createName() {
   return `${prefix} ${animal}`;
 }
 
-function isSolid(x, y) {
-
-  const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)];
-  return (
-    blockedNextSpace ||
-    x >= mapData.maxX ||
-    x < mapData.minX ||
-    y >= mapData.maxY ||
-    y < mapData.minY
-  )
-}
-
-function getRandomSafeSpot() {
-  //We don't look things up by key here, so just return an x/y
-  return randomFromArray([
-    { x: 1, y: 4 },
-    { x: 2, y: 4 },
-    { x: 1, y: 5 },
-    { x: 2, y: 6 },
-    { x: 2, y: 8 },
-    { x: 2, y: 9 },
-    { x: 4, y: 8 },
-    { x: 5, y: 5 },
-    { x: 5, y: 8 },
-    { x: 5, y: 10 },
-    { x: 5, y: 11 },
-    { x: 11, y: 7 },
-    { x: 12, y: 7 },
-    { x: 13, y: 7 },
-    { x: 13, y: 6 },
-    { x: 13, y: 8 },
-    { x: 7, y: 6 },
-    { x: 7, y: 7 },
-    { x: 7, y: 8 },
-    { x: 8, y: 8 },
-    { x: 10, y: 8 },
-    { x: 8, y: 8 },
-    { x: 11, y: 4 },
-  ]);
-}
-
 
 (function () {
 
@@ -121,77 +49,38 @@ function getRandomSafeSpot() {
   let playerRef;
   let players = {};
   let playerElements = {};
-  let coins = {};
-  let coinElements = {};
 
   const gameContainer = document.querySelector(".game-container");
   const playerNameInput = document.querySelector("#player-name");
   const playerColorButton = document.querySelector("#player-color");
 
 
-  function placeCoin() {
-    const { x, y } = getRandomSafeSpot();
-    const coinRef = firebase.database().ref(`coins/${getKeyString(x, y)}`);
-    coinRef.set({
-      x,
-      y,
-    })
-
-    const coinTimeouts = [2000, 3000, 4000, 5000];
-    setTimeout(() => {
-      placeCoin();
-    }, randomFromArray(coinTimeouts));
-  }
-
-  function attemptGrabCoin(x, y) {
-    const key = getKeyString(x, y);
-    if (coins[key]) {
-      // Remove this key from data, then uptick Player's coin count
-      firebase.database().ref(`coins/${key}`).remove();
-      playerRef.update({
-        coins: players[playerId].coins + 1,
-      })
-    }
-  }
-
-
-  function handleArrowPress(xChange = 0, yChange = 0) {
+  function handleArrowPress(xChange = 0) {
     const newX = players[playerId].x + xChange;
-    const newY = players[playerId].y + yChange;
+
     if (!isSolid(newX, newY)) {
       //move to the next space
       players[playerId].x = newX;
-      players[playerId].y = newY;
-      if (xChange === 1) {
-        players[playerId].direction = "right";
-      }
-      if (xChange === -1) {
-        players[playerId].direction = "left";
-      }
       playerRef.set(players[playerId]);
-      attemptGrabCoin(newX, newY);
     }
   }
 
   function initGame() {
 
-    new KeyPressListener("ArrowUp", () => handleArrowPress(0, -1))
-    new KeyPressListener("ArrowDown", () => handleArrowPress(0, 1))
-    new KeyPressListener("ArrowLeft", () => handleArrowPress(-1, 0))
-    new KeyPressListener("ArrowRight", () => handleArrowPress(1, 0))
+    new KeyPressListener("ArrowLeft", () => handleArrowPress(-1))
+    new KeyPressListener("ArrowRight", () => handleArrowPress(1))
 
     const allPlayersRef = firebase.database().ref(`players`);
-    const allCoinsRef = firebase.database().ref(`coins`);
 
     allPlayersRef.on("value", (snapshot) => {
       //Fires whenever a change occurs
       players = snapshot.val() || {};
       Object.keys(players).forEach((key) => {
         const characterState = players[key];
+
         let el = playerElements[key];
         // Now update the DOM
         el.querySelector(".Character_name").innerText = characterState.name;
-        el.querySelector(".Character_coins").innerText = characterState.coins;
         el.setAttribute("data-color", characterState.color);
         el.setAttribute("data-direction", characterState.direction);
         const left = 16 * characterState.x + "px";
@@ -276,18 +165,6 @@ function getRandomSafeSpot() {
       })
     })
 
-    //Update player color on button click
-    playerColorButton.addEventListener("click", () => {
-      const mySkinIndex = playerColors.indexOf(players[playerId].color);
-      const nextColor = playerColors[mySkinIndex + 1] || playerColors[0];
-      playerRef.update({
-        color: nextColor
-      })
-    })
-
-    //Place my first coin
-    placeCoin();
-
   }
 
   firebase.auth().onAuthStateChanged((user) => {
@@ -300,24 +177,17 @@ function getRandomSafeSpot() {
       const name = createName();
       playerNameInput.value = name;
 
-      const { x, y } = getRandomSafeSpot();
-
-
       `eartype: randomFromArray(playerEar),
         bodytype: randomFromArray(playerBody),`
+
       playerRef.set({
         id: playerId,
         name,
-        direction: "right",
-        color: randomFromArray(playerColors),
         x,
-        y,
-        coins: 0,
       })
 
       //Remove me from Firebase when I diconnect
       playerRef.onDisconnect().remove();
-
       //Begin the game now that we are signed in
       initGame();
     } else {
